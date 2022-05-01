b078d534886dfaafda3c521063df8eba73e18c27
Akshay2Agarwal <akshay.agarwal@grofers.com>
2022-05-01 22:45:45 +0530

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -84,8 +84,8 @@ fn main() {
         generations += 1;
         println!("{}", clear::All);
         display_world(world);
-        println!("{blue}Population at generation {g} is {c}", blue =
-        color::Fg(color::Blue), g = generations, c = census(world)); thread::sleep(time::Duration::from_secs(2));        
+        println!("{blue}Population at generation {g} is {c}", blue = color::Fg(color::Blue), g = generations, c = census(world));
+        thread::sleep(time::Duration::from_secs(2));        
     }
 
 
